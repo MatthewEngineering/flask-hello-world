1e07dbb07f2941b206d973e70f88993469ca1535
Code7 <67502293+MatthewEngineering@users.noreply.github.com>
2023-09-07 03:09:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from dash import Dash, Input, Output, State, callback, dash_table, dcc, html
+import dash_bootstrap_components as dbc
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+first_page_layout = dbc.Container([
+    dbc.Label('Select a Stock'),  # Update label
+    dbc.Input(id='underlying-input', placeholder='Enter underlying symbol', type='text',value="RIVN"),
+    dbc.Button('Load Options', id='load-options-button', n_clicks=1, color='primary'),
+    dash_table.DataTable(id='tbl'),
+    dbc.Alert(id='tbl_out'),
+])
+
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
